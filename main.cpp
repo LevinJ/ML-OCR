231e1f03d18046b97f87b7c600cac1cc57df2dc7
LevinJ <jianzhirong@gmail.com>
2014-10-17 21:23:32 +0800

    get the decision tree work, error rate 38.7%

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "SvmTest.h"
 #include "mnist-knn/Knnopencv.h"
 #include "mnist-svm/Svmopencv.h"
+#include "mnist-decisiontree/DTopencv.h"
 #include "mnist-nn/NNopencv.h"
 
 using namespace cv;
@@ -26,7 +27,8 @@ void test(){
 	start = clock();
 	
 	//Svmopencv obj;
-	NNopencv obj;
+	//NNopencv obj;
+	DTopencv obj;
 	obj.test();
 	diff = (std::clock() - start) / (double)CLOCKS_PER_SEC;
 	printf("\nOverall Duration:%.0f(Seconds)", diff);
---
+++ mnist-decisiontree/DTopencv.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/ml/ml.hpp>
+#include <stdio.h>
+#include <ctype.h>
+//#include <stdlib.h>
+#include <ctime>
+
+typedef unsigned char       BYTE;
+using namespace cv;
+using namespace std;
+
+
+class DTopencv
+{
+public:
+	void extractTrainingData(int& numImages, CvMat*&trainingVectors, CvMat*& trainingLabels);
+	void extractTestingData(int& numImages, CvMat*&testVectors, CvMat*& actualLabels);
+	int readFlippedInteger(FILE *fp);
+	CvDTree* mnist_create_dtree(const CvMat* data, const CvMat* missing,
+		const CvMat* responses, float p_weight);
+	void test();
+	DTopencv();
+	~DTopencv();
+};
+
